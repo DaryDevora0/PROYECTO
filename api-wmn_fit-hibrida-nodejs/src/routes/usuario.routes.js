abda7f81b3cd1783491c023e3a783cf2821180be
Eduardo Esquivel <93093856+dardo17@users.noreply.github.com>
2026-04-11 15:47:52 -0600

    Update usuario.routes.js

--- api-wmn_fit-hibrida-nodejs/src/routes/usuario.routes.js
+++ api-wmn_fit-hibrida-nodejs/src/routes/usuario.routes.js
@@ -1,18 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const UsuarioController = require('../controllers/usuario.controller');
-router.get('/', UsuarioController.obtenerUsuarios);  // todos
-router.get('/:id', UsuarioController.obtenerUsuarioPorId); // uno por id
-router.post('/', UsuarioController.crearUsuario);
-router.put('/:id', UsuarioController.actualizarUsuario);
-router.delete('/:id', UsuarioController.eliminarUsuario);
 
-
-// login
-router.post('/login', UsuarioController.login);
-router.post('/logout', UsuarioController.logout);
-
-
-
-
-module.exports = router;
